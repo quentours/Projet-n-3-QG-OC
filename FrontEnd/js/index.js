dcb32782eadaaf4edd547bfa2887de9d37697bed
Quentin Guihard <quentin.guihard@outlook.fr>
2024-03-17 13:16:42 +0100

    Ajout des boutons filtres

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,12 +1,21 @@
+let works;
+
 // Récupération des travaux depuis l'API
 async function getCollection() {
 const reponse = await fetch("http://localhost:5678/api/works");
-const works = await reponse.json();
+works = await reponse.json();
 
 genererCollection(works);
 }
 
 function genererCollection(works) {
+    if (!works || works.length ===0) {
+        console.error("La collection est vide ou non définit");
+        return
+    }
+
+
+
     for (let i=0; i < works.length; i++) {
         const fiche = works[i];
         // Récupérationd de l'élément DOM qui accueillera les différents projet
@@ -33,3 +42,43 @@ function genererCollection(works) {
 }
 
 getCollection()
+
+// Gestions des boutons
+
+const boutonTous = document.querySelector(".btn-tous");
+
+boutonTous.addEventListener("click" ,function() {
+    document.querySelector (".gallery").innerHTML="";
+    genererCollection(works);
+    });
+    
+
+const boutonObjet = document.querySelector(".btn-objet");
+
+boutonObjet.addEventListener("click" ,function() {
+    const ProjetFiltres = works.filter(function (work) {
+        return work.category.id === 1;
+    });
+    document.querySelector (".gallery").innerHTML="";
+    genererCollection(ProjetFiltres);
+})
+
+const boutonAppartement = document.querySelector(".btn-appartement");
+
+boutonAppartement.addEventListener("click" ,function() {
+    const ProjetFiltres = works.filter(function (work) {
+        return work.category.id === 2;
+    });
+    document.querySelector (".gallery").innerHTML="";
+    genererCollection(ProjetFiltres);
+})
+
+const boutonHotel = document.querySelector(".btn-hotel");
+
+boutonHotel.addEventListener("click" ,function() {
+    const ProjetFiltres = works.filter(function (work) {
+        return work.category.id === 3;
+    });
+    document.querySelector (".gallery").innerHTML="";
+    genererCollection(ProjetFiltres);
+})
